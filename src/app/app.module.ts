69c303e1324e9681943531f2f13ab192640be64e
Ignacio Saurin <nachosaurin@gmail.com>
2023-02-03 12:01:24 -0300

    Agregado de nueva pagina Sobre mi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { TrabajosComponent } from './componentes/trabajos/trabajos.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    ContactoComponent,
+    TrabajosComponent,
+    EducacionComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
